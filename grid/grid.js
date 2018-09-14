6db31234b5a859e28ec111a4191b2cc87eb04acf
David Adeola <adeoladavid15@gmail.com>
2018-09-14 16:37:55 +0300

    Created a dispatcher to check if a link has been clicked and gets that link

--- grid/grid.js
+++ grid/grid.js
@@ -3,4 +3,10 @@ document.addEventListener("DOMContentLoaded", function(){
   var lastClickedElement;//declare a variable called lastClickedElement
   Barba.Pjax.init();//Start Barba
   Barba.Prefetch.init();//Initialize Barba.Prefetch
+});
+
+Barba.Dispatcher.on('linkClicked', function(el){
+  //Creates an event that checks for the specific link clicked
+  lastClickedElement=el; // sets lastClickedElement to that link;
+
 })
