8dc395779fa2469a1ed2baa797eefad0214be1b1
David Adeola <adeoladavid15@gmail.com>
2018-09-14 16:32:44 +0300

    Initialize Barba and Barba.Prefetch

---
+++ grid/grid.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(){
+  //check while the document is loading to the below
+  var lastClickedElement;//declare a variable called lastClickedElement
+  Barba.Pjax.init();//Start Barba
+  Barba.Prefetch.init();//Initialize Barba.Prefetch
+})
